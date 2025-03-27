0567babe569d874e1c07ad7ba68d54e669e73c9e
Chieko Natori <chieko.natori@gmail.com>
2025-03-27 15:20:51 +0800

    Add window size control for desktop app

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:desktop_window/desktop_window.dart';
 import 'package:universal_platform/universal_platform.dart';
 
+Future setDesktopWindow() async {
+  await DesktopWindow.setWindowSize(const Size(412, 983));
+}
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized(); // Initializing Flutter binding (might not necessary but I put for best practice)
+
+  if (UniversalPlatform.isDesktop) {
+    setDesktopWindow();
+  }
+  
   runApp(const MyApp());
 }
 
